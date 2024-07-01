9d2d96698a0b64cb6d4e3ace06e719372c5f9b8d
Ken Harmon <harmkenn@gmail.com>
2024-07-01 17:28:55 -0600

    Update

---
+++ kivy_venv/MyApps/App04/main.py
@@ -0,0 +1,12 @@
+# https://www.youtube.com/watch?v=dLgquj0c5_U&list=PLCC34OHNcOtpz7PJQ7Tv7hqFBP_xDDjqg
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello World')
+    
+if __name__ == '__main__':
+    MyApp().run()
